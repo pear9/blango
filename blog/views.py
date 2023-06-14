d947de7374756b36247599f9ae555aa98e798133
pear9 <rosepear9@gmail.com>
2023-06-14 15:59:17 +0545

    urls added

--- blog/views.py
+++ blog/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, "blog/index.html")
+
+def get_ip(request):
+  from django.http import HttpResponse
+  return HttpResponse(request.META['REMOTE_ADDR'])
